e046051cc544f49ccee6770934405e224262f40f
Alex Rios <alexrios.dev@gmail.com>
2020-11-17 14:32:12 -0600

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(value) {
+  if (value <= 400) {
+    return 'This one is on me!';
+  }
+  else if (2000 < value && value < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (value > 2500) {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(destination) {
+  return destination === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous': {
+      return 'Thank you so much.';
+      break;
+    }
+    case 'not as generous': {
+      return 'Thank you.';
+      break;
+    }
+    default: {
+      return 'Bye.';
+    }
+  }
 }
